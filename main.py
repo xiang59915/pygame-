5f231d44764cf1a87ac11f288d1255b57d27ee0d
其实我叫有人 <42315439+xiang59915@users.noreply.github.com>
2019-01-03 22:05:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Star Pusher (a Sokoban clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-
 import random, sys, copy, os, pygame, manager
 from pygame.locals import *
 from manager.havefun import *
